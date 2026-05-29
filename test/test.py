b1c6cf82f69fc94d59d451279e23cf1caad4cfa9
CambridgeinstitutetechnologyBLR-Aishu <aishwarya.24ece@cambridge.edu.in>
2026-05-29 15:42:54 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -4,25 +4,15 @@ from cocotb.triggers import RisingEdge
 
 @cocotb.test()
 async def test_nlfsr(dut):
-    dut._log.info("Starting NLFSR PQC Simulation")
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
-
-    # Reset sequence
     dut.rst_n.value = 0
     await RisingEdge(dut.clk)
     dut.rst_n.value = 1
     dut.ena.value = 1
-
-    # Load initial seed
-    dut.ui_in.value = 0xAA
-    dut.uio_in.value = 0x01 # Set Load bit
+    dut.ui_in.value = 0x55
+    dut.uio_in.value = 0x01
     await RisingEdge(dut.clk)
-    
-    # Run NLFSR
-    dut.uio_in.value = 0x02 # Set Enable bit
+    dut.uio_in.value = 0x02
     await RisingEdge(dut.clk)
-    
-    # Check that output is not the same as input
-    assert dut.uo_out.value != 0xAA
-    dut._log.info("PQC NLFSR is functional!")
+    assert dut.uo_out.value != 0x55
